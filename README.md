8aef33b422274fc23e89063a6a5589bd09d49b40
FyrnDly <fadramdani25@gmail.com>
2023-09-10 21:47:32 +0700

    [FIRST] Template Style & Script

---
+++ README.md
@@ -0,0 +1,9 @@
+Slicing [Front-End] Website
+<img src="assets/icon/karaven_txt.svg">
+===
+Membuat tampilan website yang akan ditampilkan kepada user pada web <b>KaraVen</b>. Website ini berfungsi memutar musik yang diinginkan user untuk karaoke. Pada branch ini berisi beberapa halaman, diantaranya:
+- <b>Halaman Landing Page</b>, tampilan awal ketika user baru membuka website yang berisi
+    - Fitur pencarian musik
+    - Pilihan lagu berdasarkan judul, genre, playlist, dan artis
+- <b>Halaman Hasil Pencarian</b>, tampilan ketika user melakukan pencarian musik yang akan menampilkan pilihan lagu berdasarkan pencarian tersebut dan rekomendasi lagu yang sesuai dengan pencarian
+- <b>Halaman Pemutaran Musik</b>, tampilan pemutaran musik yang dipilih user dengan menggunakan sistem embed dari lagu yang diupload pada youtube
---
+++ script/main.js
@@ -0,0 +1,51 @@
+// function untuk menukar form dari header ke nav
+function moveForm() {
+    // Manggil element
+    var header = document.querySelector("#headerForm"); 
+    var form = document.querySelector("form.search");
+    var navbar = document.querySelector("#togglerNavbar");
+    var navbarItems = document.querySelector("ul.navbar-nav");
+    // ukur tinggi header
+    var headerHeight = header.offsetHeight;
+    // ukur posisi scroll user
+    var scrollPosition = window.pageYOffset;
+    if (scrollPosition > headerHeight) {
+        // ubah class form header untuk navbar
+        form.classList.remove('col-md-8','col-lg-6');
+        form.classList.add('d-flex');
+        // ubah posisi navbar-nav items dari pojok kanan ke kiri
+        navbarItems.classList.remove('ms-auto');
+        navbarItems.classList.add('me-auto');
+        // pindahin form ke navbar
+        navbar.appendChild(form);
+    } else {
+        // ubah class form header untuk navbar
+        form.classList.remove('d-flex');
+        form.classList.add('col-md-8', 'col-lg-6');
+        // ubah posisi navbar-nav items dari pojok kiri ke kanan
+        navbarItems.classList.add('ms-auto');
+        navbarItems.classList.remove('me-auto');
+        // pindahin form ke header kembali
+        header.appendChild(form);
+    }
+}
+// Menjalankan fungsi moveForm saat window di-scroll
+window.addEventListener("scroll", moveForm);
+
+// Cek posisi footer dan tambahin maring bottom untuk fixed top navbar
+window.addEventListener('load', function () {
+    // ambil element footer dan ukuran footer
+    var footer = document.querySelector('footer');
+    var footerPosition = footer.offsetTop;
+    var footerSizeHeight = footer.offsetHeight;
+    var checkBottom = window.innerHeight - footerPosition;
+    // tambahin fixed bottom kalau posisi buttom tidak dipaling bawah window screen
+    if (checkBottom >= footerSizeHeight) {
+        footer.classList.add('fixed-bottom');
+    };
+    // ambil element nav dan ukuran nav
+    var nav = document.querySelector('nav');
+    var navSizeHeight = nav.offsetHeight;
+    var body = document.querySelector('body');
+    body.style.paddingBottom = navSizeHeight + 'px';
+});
